aac86c3c061f8b8ddcae853764585ea732161b92
Matías Pierobón Marcos <matias.pierobonm@gmail.com>
2018-09-13 14:36:42 -0300

    refactor products controller

--- src/controllers/preducts.js
+++ src/controllers/preducts.js
@@ -1,22 +1,5 @@
-const Type = require('../models/Type');
 const Product = require('../models/Product');
-const status = require('http-status');
 
-module.exports = app => {
-  app.get('/', async (req, res, next) => {
-    try {
-      const products = await Product.find({}, '-__v').populate('type');
-      res.status(status.OK).json(products);
-    } catch (error) {
-      next(error);
-    }
-  });
-  app.post('/', async (req, res, next) => {
-    try {
-      const product = await Product.create(req.body);
-      res.status(status.CREATED).json(product);
-    } catch (error) {
-      next(error);
-    }
-  });
-};
+exports.all = async () => Product.find({}).populate('type');
+
+exports.create = async (args) => product.create(args);
