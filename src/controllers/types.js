9352eecb90f4ab8d352fda13c91c25f330025f4b
Matías Pierobón Marcos <matias.pierobonm@gmail.com>
2018-09-13 14:36:50 -0300

    refactor type controller

--- src/controllers/types.js
+++ src/controllers/types.js
@@ -1,21 +1,5 @@
 const Type = require('../models/Type');
-const status = require('http-status');
 
-module.exports = app => {
-  app.get('/types', async (req, res, next) => {
-    try {
-      const products = await Type.find({});
-      res.status(status.OK).json(products);
-    } catch (error) {
-      next(error);
-    }
-  });
-  app.post('/types', async (req, res, next) => {
-    try {
-      const product = await Type.create(req.body);
-      res.status(status.CREATED).json(product);
-    } catch (error) {
-      next(error);
-    }
-  });
-};
+exports.all = async () => Type.find({});
+
+exports.create = async (args) => Type.create(args);
