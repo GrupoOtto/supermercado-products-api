f3e28a00c2f21c5c7e605f53ccf8dbad03af6002
Matías Pierobón Marcos <matias.pierobonm@gmail.com>
2018-09-13 14:37:20 -0300

    use api as getway

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
-const controller_uno = require('./controllers/preducts');
-const controller_y_dos = require('./controllers/types');
+const api = require('./api');
 
 mongoose.connect('mongodb://stock-db/stock');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use((err, req, res, next) => {
   reject(new Error('Something went wrong!, err:' + err));
   res.status(500).send('Something went wrong!');
 });
-controller_uno(app);
-controller_y_dos(app);
+
+api(app);
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
