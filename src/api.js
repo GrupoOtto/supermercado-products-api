6715202b7873429b533d3c4ce7356356b9dcf594
Matías Pierobón Marcos <matias.pierobonm@gmail.com>
2018-09-19 18:42:56 -0300

    add validators for post actions

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 const status = require('http-status');
+const validate = require('express-validation');
 const products = require('./controllers/preducts');
 const types = require('./controllers/types');
+const productValidator = require('./validators/products');
+const typeValidator = require('./validators/types');
 
 const handle = fn => (req, res, next) => fn(req, res, next).catch(next);
 
@@ -10,7 +13,7 @@ module.exports = app => {
     res.status(status.OK).json(await products.all())
   }));
 
-  app.post('/', handle(async (req, res) => {
+  app.post('/', validate(productValidator), handle(async (req, res) => {
     const product = await products.create(req.body);
     res.status(status.CREATED).json(product);
   }));
@@ -19,7 +22,7 @@ module.exports = app => {
     res.status(status.OK).json(await types.all())
   }));
 
-  app.post('/types', handle(async (req, res) => {
+  app.post('/types', validate(typeValidator), handle(async (req, res) => {
     const type = await types.create(req.body);
     res.status(status.CREATED).json(type);
   }));
