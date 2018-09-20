820351fa7c4a96dd5c18b1425a6db7ca97157a6d
franco borrelli <francoborrelli96@outlook.com>
2018-09-20 18:30:54 -0300

    create resource builder

---
+++ src/utils/resource.js
@@ -0,0 +1,30 @@
+const status = require('http-status');
+const validate = require('express-validation');
+const path = require('path');
+
+const handle = fn => (req, res, next) => fn(req, res, next).catch(next);
+
+module.exports = (app) => (base, controller, validator) => {
+  app.get(base, validate(validator.all), handle(async (req, res) => {
+    res.status(status.OK).json(await controller.all())
+  }));
+
+  app.post(base, validate(validator.create), handle(async (req, res) => {
+    res.status(status.CREATED).json(await controller.create(req.body));
+  }));
+
+  app.get(path.join(base, ':id'), validate(validator.get), handle(async (req, res) => {
+    const { id } = req.params;
+    res.status(status.OK).json(await controller.get(id))
+  }));
+
+  app.put(path.join(base, ':id'), validate(validator.update), handle(async (req, res) => {
+    const { body } = req;
+    res.status(status.OK).json(await controller.update(body));
+  }));
+
+  app.delete(path.join(base, ':id'), validate(validator.delete), handle(async (req, res) => {
+    const { body } = req;
+    res.status(status.OK).json(await controller.update(body));
+  }));
+}
