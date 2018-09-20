40d0fab5e2fb0872db54faa0b90b60a788fe3b0d
franco borrelli <francoborrelli96@outlook.com>
2018-09-20 18:56:23 -0300

    Add findByType to product

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+const status = require('http-status');
 const products = require('./controllers/products');
 const types = require('./controllers/types');
 const productValidator = require('./validators/products');
@@ -6,10 +7,17 @@ const typeValidator = require('./validators/types');
 const handle = fn => (req, res, next) => fn(req, res, next).catch(next);
 
 module.exports = app => {
-
   const resource = require('./utils/resource')(app);
 
   resource('/types', types, typeValidator);
   resource('/', products, productValidator);
 
-}
+  app.get(
+    '/types/:id/products',
+    handle(async (req, res) => {
+      const { id } = req.params;
+      res.status(status.OK).json(await products.findByType(id));
+    })
+  );
+
+};
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,6 +1,6 @@
 const Product = require('../models/Product');
 
-exports.all = async () => Product.find({}).populate('type');
+exports.all = async (args) => Product.find(args).populate('type');
 
 exports.get = async _id => Product.findById({ _id }).populate('type');
 
@@ -9,3 +9,6 @@ exports.update = async (_id, args) => Product.findOneAndUpdate({ _id }, args);
 exports.delete = async _id => Product.deleteOne({ _id });
 
 exports.create = async args => Product.create(args);
+
+exports.findByType = async type => Product.find({type}, "-type");
+
