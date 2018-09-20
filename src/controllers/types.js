87f0bef5bf1eb6c4f964141fb20f5b9c2160715d
franco borrelli <francoborrelli96@outlook.com>
2018-09-20 18:31:30 -0300

    add actions to type ctrl

--- src/controllers/types.js
+++ src/controllers/types.js
@@ -2,4 +2,10 @@ const Type = require('../models/Type');
 
 exports.all = async () => Type.find({});
 
-exports.create = async (args) => Type.create(args);
+exports.create = async args => Type.create(args);
+
+exports.get = async _id => Type.findById({ _id }).populate('type');
+
+exports.update = async (_id, args) => Type.findOneAndUpdate({ _id }, args);
+
+exports.delete = async _id => Type.deleteOne({ _id });
