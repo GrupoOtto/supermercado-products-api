0ef19b35b74b9942ffeb5c607f85a2a22515cdc2
Franco Borrelli <francoborrelli96@outlook.com>
2018-10-24 13:23:10 -0300

    Fix bug when showing object after updating it

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -4,11 +4,10 @@ exports.all = async (args) => Product.find(args).populate('type');
 
 exports.get = async _id => Product.findById({ _id }).populate('type');
 
-exports.update = async (_id, args) => Product.findOneAndUpdate({ _id }, args);
+exports.update = async (_id, args) => Product.findOneAndUpdate({ _id }, args, {new: true});
 
 exports.delete = async _id => Product.deleteOne({ _id });
 
 exports.create = async args => Product.create(args);
 
 exports.findByType = async type => Product.find({type}, "-type");
-
